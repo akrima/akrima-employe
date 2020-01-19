4b73076406aad0bb9ef61b4edc5fddac9995e097
Abderrahim KRIMA <stayak1@free.fr>
2020-01-19 13:25:01 -0500

    commit5

--- src/main/java/com/akrima/contact/DataLoaderComponent.java
+++ src/main/java/com/akrima/contact/DataLoaderComponent.java
@@ -1,12 +1,18 @@
 package com.akrima.contact;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.bson.BsonBinarySubType;
+import org.bson.types.Binary;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.akrima.contact.documents.Employe;
 import com.akrima.contact.repositories.EmployeReactiveRepository;
@@ -21,6 +27,11 @@ public class DataLoaderComponent {
 
 	private final EmployeReactiveRepository employeReactiveRepository;
 
+	private Resource loadPhoto(int index) {
+	    return new ClassPathResource("images/"+index+".jpeg");
+	}
+	
+	
 	public Flux<Employe> createEmployee() {
 		
 		Faker faker= new Faker();
@@ -30,15 +41,22 @@ public class DataLoaderComponent {
 		List<Employe> employees=new ArrayList<>();
 		
 		for(int i=0; i<50;i++) {
-			Employe employe = Employe.builder().nom(faker.name().lastName()).prenom(faker.name().firstName())
-					.phoneNumber(faker.phoneNumber().cellPhone())
-					.dateNaissance(faker.date().birthday())
-					.dateEmbauche(faker.date().between(date1, date2))
-					.office(faker.country().capital())
-					.salary(Float.valueOf(faker.commerce().price(1000, 3000).replace(",", ".")))
-					.position(faker.job().position())
-					.build();
-			employees.add(employe);
+			Employe employe;
+			try {
+				employe = Employe.builder().nom(faker.name().lastName()).prenom(faker.name().firstName())
+						.phoneNumber(faker.phoneNumber().cellPhone())
+						.dateNaissance(faker.date().birthday())
+						.dateEmbauche(faker.date().between(date1, date2))
+						.office(faker.country().capital())
+						.salary(Float.valueOf(faker.commerce().price(1000, 3000).replace(",", ".")))
+						.position(faker.job().position())
+						.photo(new Binary(BsonBinarySubType.BINARY, Files.readAllBytes(loadPhoto(i).getFile().toPath())))
+						.build();
+				employees.add(employe);
+			} catch (IllegalArgumentException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return employeReactiveRepository.saveAll(employees);
 	}
